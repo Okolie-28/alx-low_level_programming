5034d3181f6ad43d99ada4c88277410c7422a0b6
Okolie-28 <okolieesther28@gmail.com>
2023-04-04 20:02:34 -0700

    pointers, arrays and strings

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
- *_memset - fill the block of memory with a specific value
+ * _memset - fill the block of memory with a specific value
  * @s: starting address of memory to be filled
- * @n: number of bytes to be changed
- * @b: input variable of char type represents
+ * @b: input variable of char type represent
  * the character to fill s
+ * @n: number of bytes to be changed
  * Return: changed array with new value of n bytes
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int r = 0;
+	int i = 0;
 
-	for (n > 0; r++)
+	for (; n > 0; i++)
 	{
-		s[r] = b;
+		s[i] = b;
 		n--;
 	}
 	return (s);
--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *_memcpy - a function that copies memory area
+ * _memcpy - a function that copies memory area
  * @dest: memory where is stored
  * @src: memory where is copied
  * @n: number of bytes
@@ -11,7 +11,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	int r = 0;
 	int i = n;
 
-	for (r < i; r++)
+	for (; r < i; r++)
 	{
 		dest[r] = src[r];
 		n--;
