f380c2eb2e66743e10c85488666fc46c23c5b5eb
Okolie-28 <okolieesther28@gmail.com>
2023-04-12 10:00:04 -0700

    argc argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,8 +9,8 @@
  *Return: 0
  */
 
-int main(int __attribute__((unused)) argc, **argv[])
+int main(int __attribute__((unused)) argc, char *argv[])
 {
-printf("%s\n", **argv);		
+printf("%s\n", *argv);
 return (0);
 }
